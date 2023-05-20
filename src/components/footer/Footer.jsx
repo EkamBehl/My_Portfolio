e7115e089a603764ba21af2e4fa8ea0ef887c25e
EkamBehl <ekambehl@gmail.com>
2023-05-20 15:39:15 +1000

    removed dribble from footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#portfolio'>Porfolio</a></li>
-        <li><a href='#testimonials'>Testimonials</a></li>
+        
         <li><a href='#contact'>Contact</a></li>
       </ul>
       <div className="footer__socials">
       <a href='https://www.linkedin.com/in/ekambehl/' target={"_blank"}><BsLinkedin/></a>
         <a href='https://github.com/EkamBehl' target={"_blank"}><BsGithub/></a>
-        <a href='https://dribbble.com' target={"_blank"}><BsDribbble/></a>
+        
       </div>
       <div className="footer__copyright">
         <small>&copy; Ekam Behl. All rights reserved
